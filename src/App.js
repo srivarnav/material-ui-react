02ca2974096ad8e22fd81afbc9aacac47dc6375b
arnav <arnav@mapmyindia.com>
2019-08-19 18:34:17 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from './component/Login';
+import Dashboard from './component/Dashboard';
+import SignUp from './component/Signup';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/dashboard" component={Dashboard}/>
+      </Switch>
+    </Router>
   );
+}
 }
 
 export default App;
